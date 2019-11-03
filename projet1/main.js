46dc60f2dd82c525149b3d2010dc2accc7d1356e
Thao Nhu <thaonhu2710@yahoo.com>
2019-11-03 13:37:23 +0100

    2nd version

--- projet1/main.js
+++ projet1/main.js
@@ -1,70 +1,117 @@
 
-  var memoryGame = new MemoryGame();
-  var score = 0;
+document.getElementById("new-game-button").addEventListener("click", function(event) { 
+  document.querySelector(".bigsquare").innerHTML =`
+  <div class="row">
+  <div class="square grey" square-id="0"></div>
+  <div class="square grey" square-id="1"></div>
+  <div class="square grey" square-id="2"></div>
+  </div>
 
-function startGame(){
-  // reset  
-  memoryGame.randomIndexTable=[];
-    memoryGame.chooseRandomIndex();
+  <div class="row">
+  <div class="square grey" square-id="3"></div>
+  <div class="square grey" square-id="4"></div>
+  <div class="square grey" square-id="5"></div>
+  </div>
 
-    memoryGame.randomSquares.forEach(square => {
-      square.classList.remove("pink");
-      square.classList.add("grey");
-    })
-    memoryGame.randomSquares=[];
-    memoryGame.chooseSquares();
+  <div class="row">
+  <div class="square grey" square-id="6"></div>
+  <div class="square grey" square-id="7"></div>
+  <div class="square grey" square-id="8"></div>
+  </div>`;
 
-    //Pour chacun des 3 carrés sélectionnés aléatoirement, ajouter une class pour lui faire changer de couleur pendant 3 secondes
-    setTimeout(function() {
-      memoryGame.randomSquares.forEach(function(el){
-        el.classList.remove("grey");
-        el.classList.add("pink");
-      });
-
-      setTimeout(function () {
-        memoryGame.randomSquares.forEach(function(el){
-          el.classList.add("grey");
-          el.classList.remove("pink");
-          });
-
-        //setTimeout(function(){alert(`It's your turn`)},500);     
-      }, 1500)
- 
-    }, 1000);  
-}
-
-
-document.getElementById("start-button").onclick = function() {
-  startGame();
+  //reset message at each game start
+  document.querySelector(".message").innerHTML=`
+  <ul>
+  <li>The squares will appear during 2 seconds</li>
+  <li>Memorize their positions</li>
+  <li>With your excellent memory, click on their positions once they disappear</li>
+  </ul>`
   
-  var squareIdTable=[];
-  document.querySelectorAll(".square").forEach(square => { 
-    square.onclick = function () {
-      var squareId=Number(square.getAttribute("square-id"));
-      
-      if (memoryGame.randomIndexTable.indexOf(squareId) != -1) {
-      square.classList.remove("grey");
-      square.classList.add("pink");
-        if ((squareIdTable.indexOf(squareId)<0) && (squareIdTable.length<3)) {
-        squareIdTable.push(squareId);
-        }
-        if (squareIdTable.length===3){
-        alert ("You win");
-        memoryGame.renderSquares();          
-        }
-        console.log("squareId",squareId);
-        console.log("squareIdTable",squareIdTable);
-        console.log(`Index of squareId in squareIdTable`,squareIdTable.indexOf(squareId));
-        console.log(squareIdTable.length===3);
-      } else {
-      alert ("You got it wrong ! Do a new game");
-      memoryGame.renderSquares();
-      };
-      } 
-    }
+  //add start button
+  var startBtn=document.createElement("BUTTON");
+  startBtn.innerHTML="Start Game";
+  startBtn.setAttribute("id","start-button");
+  document.querySelector(".buttons").appendChild(startBtn);
 
-  )
-};
+  //when the button start appears, the button new game temporarily not visible
+  document.getElementById("new-game-button").style.display="none";
+
+  var allSquares=document.querySelectorAll(".square");
+  var memoryGame = new MemoryGame();
+  
+  function startGame(){
+      // reset table of random Index
+      memoryGame.randomIndexTable=[];
+      // before choosing new random Index
+      memoryGame.chooseRandomIndex();
+      
+      // reset format of squares randomly selected
+      memoryGame.randomSquares.forEach(square => {
+        square.classList.remove("pink");
+        square.classList.add("grey");
+      })
+      memoryGame.randomSquares=[];
+      // before choosing new random Squares
+      memoryGame.chooseSquares(allSquares);
+
+      //For each of 3 squares randomly selected, add a class to chnage its color from grey to pink during 1.5 secs
+      setTimeout(function() {
+        memoryGame.randomSquares.forEach(function(square){
+          square.classList.remove("grey");
+          square.classList.add("pink");
+          document.querySelector(".message").innerHTML="GET READY";
+        });
+  
+        setTimeout(function () {
+          memoryGame.randomSquares.forEach(function(square){
+            square.classList.add("grey");
+            square.classList.remove("pink");
+            document.querySelector(".message").innerHTML="IT'S YOUR TURN";
+            });
+    
+        }, 2000)
+   
+      }, 1000);  
+  }
+  
+  
+  document.getElementById("start-button").onclick = function() {
+    startGame();
+
+    //Ater the game starts, the start button will be removed
+    document.getElementById("start-button").parentNode.removeChild(document.getElementById("start-button"));
+
+    var squareIdTable=[];
+    document.querySelectorAll(".square").forEach(square => { 
+      square.onclick = function () {
+        var squareId=Number(square.getAttribute("square-id"));
+        
+        if (memoryGame.randomIndexTable.indexOf(squareId) != -1) {
+        square.classList.remove("grey");
+        square.classList.add("pink");
+          if ((squareIdTable.indexOf(squareId)<0) && (squareIdTable.length<3)) {
+          squareIdTable.push(squareId);
+          }
+          if (squareIdTable.length===3){
+          document.querySelector(".message").innerHTML="YOU WIN !";
+          document.querySelector(".bigsquare").innerHTML=`<div class="congrats"> <img src="congrats.gif"> </div>`;
+          //The button new game appears again as an option
+          document.getElementById("new-game-button").style.display="";
+          }
+        } else {
+        document.querySelector(".message").innerHTML="You got it wrong ! Do a new game";
+        memoryGame.renderSquares(allSquares);
+        //The button new game appears again as an option
+        document.getElementById("new-game-button").style.display="";
+        };
+        } 
+      }
+  
+    )
+  };
+});
+
+
 
 
 
--- projet1/memory.js
+++ projet1/memory.js
@@ -1,14 +1,12 @@
 class MemoryGame {
   constructor(){
-    this.allSquares=document.querySelectorAll(".square");
     this.randomSquares=[];
     this.randomIndexTable=[];
     this.index=0;
   }
 
   chooseRandomIndex () {
-    while (this.randomIndexTable.length<3){
-      
+    while (this.randomIndexTable.length<3){      
       this.index=Math.floor(Math.random()*9);
       if (this.randomIndexTable.indexOf(this.index) < 0) {     
         this.randomIndexTable.push(this.index);
@@ -17,13 +15,13 @@ class MemoryGame {
       return this.randomIndexTable;
     }
   
-  chooseSquares() {
-    this.randomIndexTable.forEach(randomIndex => {this.randomSquares.push(this.allSquares[randomIndex]);})
+  chooseSquares(allSquares) {
+    this.randomIndexTable.forEach(randomIndex => {this.randomSquares.push(allSquares[randomIndex]);})
     return this.randomSquares;
   }
 
-  renderSquares() {
-    this.allSquares.forEach(square => {
+  renderSquares(allSquares) {
+    allSquares.forEach(square => {
       square.classList.add("grey");
       square.classList.remove("pink");
     })
